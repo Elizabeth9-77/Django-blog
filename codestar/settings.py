1140a33c5965e0ccf11a4e7792f5b0d314d6e3d1
Elizabeth Travers <melocus@hotmail.co.uk>
2024-09-12 14:48:48 +0000

    moved secret key and linked settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,7 +15,11 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-
+if os.path.isfile('env.py'):
+    DEBUG=True
+else:
+    DEBUG=False
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3htiro@tl=1(0d$jc6g)4i$bla5ni#wk)m%#_=@v&+kv9cmikp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
