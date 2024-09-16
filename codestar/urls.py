3206f54654ed5340ecb789b42ad2bb34fcdd9e2c
Elizabeth Travers <melocus@hotmail.co.uk>
2024-09-16 14:47:20 +0000

    trial to see if index_view to blog_view fixes heroku

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
+from blog import views as blog_views
 
 urlpatterns = [
     path('blog/', index_views.index, name='index'),
